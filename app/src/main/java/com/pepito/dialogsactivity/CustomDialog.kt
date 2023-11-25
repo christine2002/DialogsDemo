511f1d8a21a63ceb51bc0768f14d9e7626168031
christine2002 <pepito.christine18@gmail.com>
2023-11-25 17:10:47 +0800

    version 1

---
+++ app/src/main/java/com/pepito/dialogsactivity/CustomDialog.kt
@@ -0,0 +1,30 @@
+package com.pepito.dialogsactivity
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Toast
+import com.pepito.dialogsactivity.databinding.DialogCustomBinding
+
+class CustomDialog(context: Context) : Dialog(context) {
+
+    private lateinit var binding: DialogCustomBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DialogCustomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showDialog()
+
+        binding.save.setOnClickListener {
+            Toast.makeText(context, "Your new name is " + binding.nameEditText.text, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun showDialog() {
+        val window: Window? = this.window
+        window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+}
